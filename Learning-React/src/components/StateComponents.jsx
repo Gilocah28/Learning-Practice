86816653b322a78a4d7b35966e80dcd8d016debe
Gibermar Regalado <gberxx@gmail.com>
2024-01-29 15:24:00 +0800

    UseState

--- Learning-React/src/components/StateComponents.jsx
+++ Learning-React/src/components/StateComponents.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const StateComponents = () => {
+
+  const [count, setCount] = useState(0)
+
+  // const handleIncrement = () => {
+  //   isCount(prevState => prevState + 1)
+  // }
+
+  // const handleDecrement = () => {
+  //   isCount(prevState => prevState - 1)
+  // }
+
   return (
     <div>
-      
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(prevState => prevState + 1)}>Increment</button>
+      <button onClick={() => setCount(prevState => prevState - 1)}>Decrement</button>
     </div>
   )
 }
